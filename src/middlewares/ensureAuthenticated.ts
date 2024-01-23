21998be0c31840287b19b5566995f80b57d536ca
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-01-23 19:42:12 -0300

    [fix] arrumado status code middleware auth

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -9,18 +9,18 @@ export function ensureAuthenticated(
 ) {
   const authToken = req.headers.authorization;
 
-  if (!authToken) return res.status(403).json({ error: "Sem autorização" });
+  if (!authToken) return res.status(401).json({ error: "Sem autorização" });
 
   const [, token] = authToken.split(" ");
 
   if (!jwtSecret)
-    return res.status(404).json({ error: "JWT Key não encontrada!" });
+    return res.status(401).json({ error: "JWT Key não encontrada!" });
 
   try {
     verify(token, jwtSecret);
 
     return next();
   } catch (err) {
-    return res.status(403).json({ error: "Token inválido" });
+    return res.status(401).json({ error: "Token inválido" });
   }
 }
