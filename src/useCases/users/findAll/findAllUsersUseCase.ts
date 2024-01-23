33e0bf5986bce1c8024662ae010d64c59a229b60
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-01-22 21:18:53 -0300

    [feat] implemetado dto com retorno apenas das informações necessárias

--- src/useCases/users/findAll/findAllUsersUseCase.ts
+++ src/useCases/users/findAll/findAllUsersUseCase.ts
@@ -1,14 +1,24 @@
 import { User } from "../../../entities/User";
 import { UsersRepository } from "../../../repositories/usersRepositories";
 
-type FindAllUsersResponse = User[];
+type FindAllUsersResponse = {
+  username: string;
+  email: string;
+};
 
 export class FindAllUsersUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute(): Promise<FindAllUsersResponse> {
+  async execute(): Promise<FindAllUsersResponse[]> {
     const users = await this.usersRepository.findAll();
 
-    return users;
+    const mappedUsers: FindAllUsersResponse[] = users.map(
+      (user: FindAllUsersResponse) => ({
+        username: user.username,
+        email: user.email,
+      })
+    );
+
+    return mappedUsers;
   }
 }
