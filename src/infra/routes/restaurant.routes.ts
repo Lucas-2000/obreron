a6e6003405e686c88026819eeff96d38e14919d8
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-01-25 19:34:25 -0300

    [feat] implementado middleware de autenticação nas rotas do restaurante

--- src/infra/routes/restaurant.routes.ts
+++ src/infra/routes/restaurant.routes.ts
@@ -1,20 +1,33 @@
 import { findRestaurantByUserIdFactory } from "../../useCases/restaurants/findByUserId/findRestaurantByUserIdFactory";
 import { updateRestaurantFactory } from "../../useCases/restaurants/update/updateRestaurantFactory";
 import { createRestaurantFactory } from "./../../useCases/restaurants/create/createRestaurantFactory";
+import { ensureAuthenticated } from "../../middlewares/ensureAuthenticated";
 import { Request, Response, Router } from "express";
 
 const restaurantRoutes = Router();
 
-restaurantRoutes.post("/restaurants", (req: Request, res: Response) => {
-  return createRestaurantFactory().handle(req, res);
-});
+restaurantRoutes.post(
+  "/restaurants",
+  ensureAuthenticated,
+  (req: Request, res: Response) => {
+    return createRestaurantFactory().handle(req, res);
+  }
+);
 
-restaurantRoutes.get("/restaurants", (req: Request, res: Response) => {
-  return findRestaurantByUserIdFactory().handle(req, res);
-});
+restaurantRoutes.get(
+  "/restaurants",
+  ensureAuthenticated,
+  (req: Request, res: Response) => {
+    return findRestaurantByUserIdFactory().handle(req, res);
+  }
+);
 
-restaurantRoutes.patch("/restaurants", (req: Request, res: Response) => {
-  return updateRestaurantFactory().handle(req, res);
-});
+restaurantRoutes.patch(
+  "/restaurants",
+  ensureAuthenticated,
+  (req: Request, res: Response) => {
+    return updateRestaurantFactory().handle(req, res);
+  }
+);
 
 export { restaurantRoutes };
